44dd8111fc9b9b407365f0cc92002e600c5b4273
Christian Gruber <gruber@squareup.com>
2020-05-08 23:24:40 -0700

    Make jetifier configurable.
    Allow jetifier to be configured on or off globally. Also allow an exclusion list with limited glob wildcard support.

--- test/test_workspace/javatests/foo/FooTest.java
+++ test/test_workspace/javatests/foo/FooTest.java
@@ -3,13 +3,16 @@ package foo;
 import foo.Foo.FooComponent;
 import org.junit.Test;
 
+import static com.google.common.truth.Truth.assertThat;
+
 public class FooTest {
   @Test public void testFoo() {
     Foo foo = new Foo();
     FooComponent fooComponent = FooComponent.create();
     fooComponent.bar();
 
-    Foo.Inner.builder().setFoo(foo).build();
+    Foo.Inner inner = Foo.Inner.builder().setFoo(foo).build();
+    assertThat(inner.foo()).isSameAs(foo);
   }
 
 }
