ca32b57e852dab08f769ce7f71c62cad0f169abf
Christian Gruber <gruber@squareup.com>
2020-05-21 10:33:12 -0700

    Add in licenses and extract a factory instantiation into a single use.

--- kramer/src/main/kotlin/kramer/Aars.kt
+++ kramer/src/main/kotlin/kramer/Aars.kt
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2020 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
 package kramer
 
 import org.xml.sax.Attributes
@@ -10,6 +24,8 @@ import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
 import javax.xml.parsers.SAXParserFactory
+
+private val parserFactory = SAXParserFactory.newInstance()
 
 @Throws(IOException::class)
 internal fun extractPackageFromManifest(fromZip: URI): String? {
@@ -27,7 +43,6 @@ internal fun extractPackageFromManifest(fromZip: URI): String? {
         .orElse(null)
         ?.let { path ->
           val xmlText = Files.readAllLines(path).joinToString("")
-          val parserFactory = SAXParserFactory.newInstance()
           // instancing the SAXParser class
           val saxParser = parserFactory.newSAXParser()
           var customPackage: String? = null
